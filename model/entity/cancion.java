b4342626d81f31d04c681f8cc3b6d200a3c29e32
Dangelcrack <angelguerrero540@gmail.com>
2024-02-08 19:13:00 +0100

    Interfaces3

--- model/entity/cancion.java
+++ model/entity/cancion.java
@@ -1,31 +1,55 @@
-package model;
+package model.entity;
 
 import java.util.Date;
+import java.util.Objects;
 
-public class cancion extends favorito{
+public class cancion extends favorito {
+    private String autor;
+    private Date duracion;
+    private String nombreCancion;
+
     public cancion() {
-        autor="anonimo";
-        duracion=new Date("no especificada");
+        autor = "anonimo";
+        duracion = new Date("no especificada");
+        nombreCancion = getNombre();
     }
+
     public cancion(String nombre, String genero, Date f_lanzamiento, String autor, Date duracion) {
         super(nombre, genero, f_lanzamiento);
         this.autor = autor;
         this.duracion = duracion;
     }
-    private String autor;
-    private Date duracion;
+
     public String getAutor() {
         return autor;
     }
+
     public void setAutor(String autor) {
         this.autor = autor;
     }
+
     public Date getDuracion() {
         return duracion;
     }
+
     public void setDuracion(Date duracion) {
         this.duracion = duracion;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        cancion cancion = (cancion) o;
+        return Objects.equals(autor, cancion.autor) && Objects.equals(duracion, cancion.duracion) && Objects.equals(nombreCancion, cancion.nombreCancion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), autor, duracion, nombreCancion);
+    }
+
     @Override
     public String toString() {
         return "cancion{" +
@@ -36,4 +60,5 @@ public class cancion extends favorito{
                 ", f_lanzamiento=" + f_lanzamiento +
                 '}';
     }
+
 }
--- model/entity/videojuego.java
+++ model/entity/videojuego.java
@@ -1,31 +1,54 @@
-package model;
+package model.entity;
 
 import java.util.Date;
+import java.util.Objects;
 
-public class videojuego extends favorito{
+public class videojuego extends favorito {
+    private String desarrollador;
+    private boolean mundo_abierto;
+
     public videojuego() {
-         desarrollador="anonimo";
-         mundo_abierto=false;
+        desarrollador = "anonimo";
+        mundo_abierto = false;
     }
+
     public videojuego(String nombre, String genero, Date f_lanzamiento, String desarrollador, boolean mundo_abierto) {
         super(nombre, genero, f_lanzamiento);
         this.desarrollador = desarrollador;
         this.mundo_abierto = mundo_abierto;
     }
-    private String desarrollador;
-    private boolean mundo_abierto;
+
     public String getDesarrollador() {
         return desarrollador;
     }
+
     public void setDesarrollador(String desarrollador) {
         this.desarrollador = desarrollador;
     }
+
     public boolean isMundo_abierto() {
         return mundo_abierto;
     }
+
     public void setMundo_abierto(boolean mundo_abierto) {
         this.mundo_abierto = mundo_abierto;
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        videojuego that = (videojuego) o;
+        return mundo_abierto == that.mundo_abierto && Objects.equals(desarrollador, that.desarrollador);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), desarrollador, mundo_abierto);
+    }
+
     @Override
     public String toString() {
         return "videojuego{" +
